833e65b2049010c5e1f143038d89672c400120cc
LuisPinto9 <luisenriquepinto9@gmail.com>
2024-02-26 14:24:46 -0500

    Arreglo de routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 import "primeicons/primeicons.css";
-import Router from "./routes/Router";
+import Router from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Router />);
